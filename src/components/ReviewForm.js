464ac5ace16cacbc131a58709dd4aacbe99dc5e4
Victoria Gonzalez <gonzalezevic@gmail.com>
2022-08-03 12:05:22 -0700

    vg-phase-2-project

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -6,49 +6,38 @@ function ReviewForm() {
 const [name, setName]= useState('') // content of the name
 const [review, setReview]= useState('') //content of review
 const [reviews, setReviews]= useState([]) //1.objects of existing reviews + new review
-
-//2. GET request, does not need to be named as default is a GET request
-// we are using useEffect here to display an array of already existing reviews
-// we are fetching the dbjson of the existing reviews and displaying the data as a callback of SetReviews
- useEffect(()=> {
-  fetch("http://localhost:3000/Reviews")
+//useEffect
+const displayReviews=()=>reviews.map((reviews)=> 
+ <p key={reviews.name}>{reviews.name}<br></br>{reviews.review}</p>)
+useEffect(()=>{
+fetch("http://localhost:3000/Reviews")
   .then((res)=> res.json())
   .then(data=> setReviews(data))
 }, [])
+// add review to reviewS or setReviewS
 
+//updates name
+  function handleName(e){
+   e.preventDefault()
+  setName(e.target.value)
+}
+ //updates review   
+function handleReview(e){
+  setReview(e.target.value)
+}
 
-
-const displayReviews=()=>reviews.map((reviews)=> 
- <p key={reviews.name}>{reviews.name}<br></br>{reviews.review}</p>
- )
-
-// this is telling us "hey when you fetch handleSubmit, display newReview"
-const newReview =[name, review]
-
-// this function is fethcing our data in the 
 const handleSubmit= (e)=> {
-  e.preventDefault()
-
-  fetch("http://localhost:3000/Reviews", {
+e.preventDefault()
+const newReview = {review,name}
+    fetch("http://localhost:3000/Reviews", {
     method:"POST",
     headers:{ "Content-Type": "application/json"},
     body: JSON.stringify(newReview)
   })
-  
-// the next two functions will decide what is going to be displayed in the name and review section
-// we are using state to render each review and name every time its input into the review section
+  .then((res)=>res.json())
+  .then((data)=> setReviews([data, ...reviews]))
 }
-//displays name
-  function handleName(e){
-    e.preventDefault()
-  setName(e.target.value)
-}
- //displays review   
-  function handleReview(e){
-  setReview(e.target.value)
-  }
-// jsx in  which we are passing down the handle submit prop that will display our review
-// we are also adding the actual form with the submit button into the jsx
+
   return (
     <div>
         <h2 className='title'>Reviews</h2> <br/>
