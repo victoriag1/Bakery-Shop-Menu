0bd3b7f5a40fab8fbd9cad62519d7374d7a9e6e1
Victoria Gonzalez <gonzalezevic@gmail.com>
2022-10-19 12:36:44 -0700

    phase 2

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -8,11 +8,11 @@ const [review, setReview]= useState('') //content of review
 const [reviews, setReviews]= useState([]) //1.objects of existing reviews + new review
 
 //useEffect
-const displayReviews=()=>reviews.map((reviews)=> 
- <p key={reviews.name}>{reviews.name}<br></br>{reviews.review}</p>)
+const displayReviews=()=>reviews.map((review)=> 
+ <p key={review.name}>{review.name}<br></br>{review.review}</p>)
  
 useEffect(()=>{
-fetch("http://localhost:3000/Reviews")
+  fetch("http://localhost:3000/Reviews")
   .then((res)=> res.json())
   .then(data=> setReviews(data))
 }, [])
@@ -29,9 +29,9 @@ function handleReview(e){
 }
 
 const handleSubmit= (e)=> {
-e.preventDefault()
-const newReview = {review,name}
-    fetch("http://localhost:3000/Reviews", {
+  e.preventDefault()
+  const newReview = {review,name}
+  fetch("http://localhost:3000/Reviews", {
     method:"POST",
     headers:{ "Content-Type": "application/json"},
     body: JSON.stringify(newReview)
@@ -42,15 +42,15 @@ const newReview = {review,name}
 
   return (
     <div>
-        <h2 className='title'>Reviews</h2> <br/>
-    <form className='reviewForm' onSubmit={handleSubmit}>
-      <label htmlFor='name'> Name:</label>
-      <input onChange={handleName} type="text" name ="name" id="name" placeholder="Your Name Here .." value={name}></input> <br/> <br/>
-      <label htmlFor='Review'>Review:</label>
-      <input onChange={handleReview} type= "text" placeholder="Your Review Here..." id="reviewform" value={review}></input> <br/> <br/>
-      <button type="submit">Submit Review</button> 
-    </form>
-     <h2 className='oldReviews'>{displayReviews()}</h2>
+      <h2 className='title'>Reviews</h2> <br/>
+      <form className='reviewForm' onSubmit={handleSubmit}>
+        <label htmlFor='name'> Name:</label>
+        <input onChange={handleName} type="text" name ="name" id="name" placeholder="Your Name Here .." value={name}></input> <br/> <br/>
+        <label htmlFor='Review'>Review:</label>
+        <input onChange={handleReview} type= "text" placeholder="Your Review Here..." id="reviewform" value={review}></input> <br/> <br/>
+        <button type="submit">Submit Review</button> 
+      </form>
+      <h2 className='oldReviews'>{displayReviews()}</h2>
     </div>
   )
   }
